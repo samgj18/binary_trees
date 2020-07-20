d6b25c853b1621e9eb01b509fc7920d80bd5bf02
samgj18 <samgj_18@hotmail.com>
2020-07-20 11:39:47 -0500

    Write a function that checks if a node is a leaf

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,40 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  3-binary_tree_delete.c
+ *
+ *    Description:  Function that checks if a node is a leaf
+ *
+ *        Version:  1.0
+ *        Created:  20/07/2020 10:32:17
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Juliana Chois - Samuel Gómez
+ *   Organization:  Holberton
+ *
+ * =====================================================================================
+ */
+
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - delete a tree
+ * @tree: input root
+ */
+void post_order_traversal(binary_tree_t *root)
+{
+	if (!root)
+		return;
+
+	if(root)
+	{
+		post_order_traversal(root->left);
+		post_order_traversal(root->right);
+	}
+	free(root);
+}
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	post_order_traversal(tree);
+}
---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,17 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  4-binary_tree_is_leaf.c
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  20/07/2020 11:38:36
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  SAMUEL GOMEZ JIMENEZ (samgj18), samgomjim.18@gmail.com
+ *   Organization:  Holberton
+ *
+ * =====================================================================================
+ */
