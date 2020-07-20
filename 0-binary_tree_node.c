786f42378cf60c7b1df9e074414663bbef2fe375
samgj18 <samgj_18@hotmail.com>
2020-07-19 19:00:39 -0500

    Creating first point

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,28 @@
+/*
+ * ===================================================
+ *
+ *       Filename:  0-binary_tree_node.c
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  19/07/2020 18:46:20
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Juliana Chois - Samuel Gomez
+ *   Organization:  Holberton
+ *
+ * ===================================================
+ */
+#include "binary_trees.h"
+/**
+ * binary_tree_node - created a binary tree node
+ * @parent: parent node
+ * @value: value of the node
+ * Return: Null on failure or ptr to new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value){
+
+}
+
