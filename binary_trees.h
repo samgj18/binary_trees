9324b31cb02b126a505856c9f0de756b7493e69e
jchois <choisjuliana@gmail.com>
2020-07-22 20:27:31 -0500

    header file updated (some unnecessary comments removed)

--- binary_trees.h
+++ binary_trees.h
@@ -27,11 +27,6 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-/*delete*/
-// binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-// binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-//int is_perfect(const binary_tree_t *tree, int d, int level);
-
 void binary_tree_print(const binary_tree_t *);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
